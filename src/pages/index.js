99780814348b23b0d265ac0ca466f4b9b3372898
t5krishn <tk95@live.ca>
2018-08-28 15:57:22 -0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Hello Everyone! My name is Thilina and I'm from Brampton, Canada. I just graduated from the University with a Biomedical Science degree. However, my love for computer science didn't fade since high school and I want to find that passion I had for it again. I've been working on freecodecamp on and off, but this hackathon will be enough motivation to complete the 2 courses. I'm excited to meet so many people and get a richer look into the fields of Computer Science. Much love!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
